286b4a2650bfc00295a964d6c3f52ad971a20f52
Gustavo Libraiz <gulibraiz@gmail.com>
2022-04-21 18:35:50 -0300

    Finalizando comunicação via http no adapter

--- server/repository/notification_repository/__init__.py
+++ server/repository/notification_repository/__init__.py
@@ -1,5 +1,4 @@
-import aiohttp.web_response
-from server.requests.client.http_client import HttpRequestClient
+import aiohttp
 from server.configuration.environment import Environment
 from server.configuration.custom_logging import get_main_logger
 from server.configuration.exceptions import RequestFailException
@@ -16,24 +15,24 @@ class NotificacaoRepository:
             'Authorization': f'Bearer {environment.OWNER_USER_TOKEN}'
         }
 
-    def __init__(self, request_client: HttpRequestClient, environment: Environment):
+    def __init__(self, environment: Environment = None):
         self.environment = environment
-        self.request_client = request_client if request_client else HttpRequestClient()
 
         self.auth_headers = self.build_auth_headers(environment)
 
     async def insere_notificacao(self, notificacao_dict: dict) -> dict:
         MAIN_LOGGER.info(f"Inserindo notificação {notificacao_dict}")
 
-        response: aiohttp.web_response.json_response = await self.request_client.request(
+        async with aiohttp.request(
             'POST',
-            f'{self.environment.MS_NOTIFICACAO_ENDPOINT}/{self.environment.MS_NOTIFICCAO_INSERT_NOTIFICACAO_PATH}',
+            f'{self.environment.MS_NOTIFICACAO_ENDPOINT}{self.environment.MS_NOTIFICACAO_INSERT_NOTIFICACAO_PATH}',
             json=notificacao_dict,
             headers=self.auth_headers
-        )
+        ) as response:
+            response: aiohttp.ClientResponse
 
-        if str(response.status)[0] != '2':
-            raise RequestFailException('Ocorreu um erro ao inserir uma notificação')
+            if str(response.status)[0] != '2':
+                raise RequestFailException('Ocorreu um erro ao inserir uma notificação')
 
-        return response.json()
+            return await response.json()
 
--- server/repository/perfil_repository/__init__.py
+++ server/repository/perfil_repository/__init__.py
@@ -1,5 +1,4 @@
-import aiohttp.web_response
-from server.requests.client.http_client import HttpRequestClient
+import aiohttp
 from server.configuration.environment import Environment
 from server.configuration.custom_logging import get_main_logger
 from server.configuration.exceptions import RequestFailException
@@ -16,37 +15,22 @@ class PerfilRepository:
             'Authorization': f'Bearer {environment.OWNER_USER_TOKEN}'
         }
 
-    def __init__(self, request_client: HttpRequestClient, environment: Environment):
+    def __init__(self, environment: Environment = None):
         self.environment = environment
-        self.request_client = request_client if request_client else HttpRequestClient()
-
         self.auth_headers = self.build_auth_headers(environment)
 
     async def insere_perfil(self, perfil_dict: dict) -> dict:
         MAIN_LOGGER.info(f"Inserindo perfil no ms_perfil {perfil_dict}")
 
-        response: aiohttp.web_response.json_response = await self.request_client.request(
-            'POST', f'{self.environment.MS_PERFIS_ENDPOINT}/{self.environment.MS_PERFIS_INSERT_PERFIL_PATH}',
+        async with aiohttp.request(
+            'POST', f'{self.environment.MS_PERFIS_ENDPOINT}{self.environment.MS_PERFIS_INSERT_PERFIL_PATH}',
             json=perfil_dict,
             headers=self.auth_headers
-        )
+        ) as response:
+            response: aiohttp.ClientResponse
 
-        if str(response.status)[0] != '2':
-            raise RequestFailException('Ocorreu um erro ao inserir uma notificação')
+            if str(response.status)[0] != '2':
+                raise RequestFailException('Ocorreu um erro ao inserir uma notificação')
 
-        return response.json()
-
-    async def insere_usuario(self, usuario_dict: dict) -> dict:
-        MAIN_LOGGER.info(f"Inserindo usuário no ms_perfil {usuario_dict}")
-
-        response: aiohttp.web_response.json_response = await self.request_client.request(
-            'POST', f'{self.environment.MS_PERFIS_ENDPOINT}/{self.environment.MS_PERFIS_INSERT_USUARIO_PATH}',
-            json=usuario_dict,
-            headers=self.auth_headers
-        )
-
-        if str(response.status)[0] != '2':
-            raise RequestFailException('Ocorreu um erro ao inserir uma notificação')
-
-        return response.json()
+            return await response.json()
 
